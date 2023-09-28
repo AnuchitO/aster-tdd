c8f2c7dff45f45fcc054aeb566073ec13fc65063
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-09-28 14:07:29 +0700

    shoudl return Fizz when input 3

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,6 +1,10 @@
 package main
 
 func FizzBuzz(n int) string {
+	if n == 3 {
+		return "Fizz"
+	}
+
 	if n == 2 {
 		return "2"
 	}
--- testgo/fizzbuzz_test.go
+++ testgo/fizzbuzz_test.go
@@ -23,3 +23,14 @@ func TestFizzBuzzShouldReturn2WhenInput2(t *testing.T) {
 		t.Errorf("got %q but want %q", got, want)
 	}
 }
+
+func TestFizzBuzzShouldReturnFizzWhenInput3(t *testing.T) {
+	input := 3
+
+	got := FizzBuzz(input)
+
+	want := "Fizz"
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
+}
