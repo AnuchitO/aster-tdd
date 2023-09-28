87ab7d6ee022c04d8651e6c00b0359e5e3702000
Nong AnuchitO <anuchit.prasertsang@gmail.com>
2023-09-28 14:16:29 +0700

    eliminate hard code

--- testgo/fizzbuzz.go
+++ testgo/fizzbuzz.go
@@ -1,17 +1,11 @@
 package main
 
+import "fmt"
+
 func FizzBuzz(n int) string {
 	if n == 3 {
 		return "Fizz"
 	}
 
-	if n == 4 {
-		return "4"
-	}
-
-	if n == 2 {
-		return "2"
-	}
-
-	return "1"
+	return fmt.Sprint(n)
 }
